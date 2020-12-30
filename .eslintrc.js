9df0efaecf4f85996d1f1f6d4a4d5ce298edc733
xichen1 <xichen.pan@gmail.com>
2020-12-30 16:32:04 -0700

    eslint bug fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   "env": {
-    "browser": true,
+    "node": true,
     "es6": true,
     "jest/globals": true
   },
@@ -21,8 +21,8 @@ module.exports = {
   "rules": {
     "indent": [
       "error",
-        2
-      ],
+      2
+    ],
     "linebreak-style": [
       "error",
       "unix"
